6eaded3e65f5ceee4c3d109933d1409bdc10fc67
JeremyLoh <jeremyloh03@gmail.com>
2022-12-04 00:54:31 +0800

    Add calendar to wallet component

---
+++ __tests__/pages/wallet.test.tsx
@@ -0,0 +1,30 @@
+import { test, expect, describe } from "vitest"
+import { render, screen } from '@testing-library/react'
+import Wallet from "../../pages/wallet"
+
+function getMonth(date: Date) {
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ]
+  return monthNames[date.getMonth()]
+}
+
+describe("wallet", () => {
+  describe("anonymous user", () => {
+    test("should show calendar with current month and year", () => {
+      render(<Wallet />)
+      const currentDate: Date = new Date()
+      const calendarHeader = `${getMonth(currentDate)} ${currentDate.getFullYear()}`
+      expect(screen.getByText(calendarHeader)).toBeInTheDocument()
+    })
+
+    test("should show current day wallet statistics", () => {
+      render(<Wallet />)
+      const currentDate: Date = new Date()
+      const walletTransactionDate = screen.getByLabelText("wallet-transaction-date")
+      expect(walletTransactionDate).toBeInTheDocument()
+      expect(walletTransactionDate)
+        .toHaveTextContent(`${currentDate.getDate()}`)
+    })
+  })
+})
--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,10 +1,19 @@
-import { FC } from "react";
+import React, { FC, useState } from "react";
+import Calendar from "react-calendar"
+
+//https://www.copycat.dev/blog/react-calendar/
 
 const Wallet:FC<any> = () => {
+  const [date, setDate] = useState<Date>(new Date())
   return (
-    <>
+    <div>
       <h1>Wallet</h1>
-    </>
+      <Calendar onChange={setDate} value={date} />
+      <div aria-label="wallet-transaction-date">
+        Date
+        {date.getDate()}
+      </div>
+    </div>
   )
 }
 
