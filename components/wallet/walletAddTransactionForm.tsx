cb940b049753f954da5608c4d8e55ce69d612a84
JeremyLoh <jeremyloh03@gmail.com>
2022-12-10 23:02:55 +0800

    Add wallet form component

---
+++ components/wallet/walletAddTransactionForm.tsx
@@ -0,0 +1,75 @@
+import React, { FC, useState } from "react"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import styles from "../../styles/components/wallet/WalletAddTransactionForm.module.css"
+
+export interface Transaction {
+  name: string,
+  amount: number,
+  transactionDate: Date
+}
+
+interface handleNewTransaction {
+  (transaction: Transaction): void
+}
+
+// TODO have a handleError function prop that can be called
+interface WalletFormProps {
+  handleNewTransaction: handleNewTransaction
+}
+
+const WalletForm:FC<WalletFormProps> = (props) => {
+  const [newTransactionDate, setNewTransactionDate] = useState<Date>(new Date())
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const body = getRequestBody(event, newTransactionDate)
+    const response = await fetch("/api/wallet/transaction/create", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const transaction = await response.json()
+    props.handleNewTransaction(transaction)
+  }
+
+  return (
+    <form className={styles.form}
+      action="/api/wallet/transaction/create"
+      method="post"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" required />
+      <label htmlFor="amount">Amount</label>
+      <input type="number" min="0" step="0.01" id="amount" name="amount" required />
+      <div />
+      <label htmlFor="transactionDate">Transaction Date</label>
+      <input type="text"
+        hidden
+        readOnly
+        value={newTransactionDate.toDateString()}
+        id="transactionDate"
+        name="transactionDate"
+      />
+      <DatePicker selected={newTransactionDate} 
+        onChange={(date: Date) => setNewTransactionDate(date)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function getRequestBody(event: React.FormEvent<HTMLFormElement>, newTransactionDate: Date) {
+  const name: string = (event.currentTarget.elements.namedItem("name") as HTMLInputElement).value
+  const amount: number = Number((event.currentTarget.elements.namedItem("amount") as HTMLInputElement).value)
+  return {
+    name,
+    amount,
+    transactionDate: newTransactionDate
+  }
+}
+
+export default WalletForm
