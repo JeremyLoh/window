98926fe054cc40612693edeb3219eef7fec750e9
JeremyLoh <jeremyloh03@gmail.com>
2022-12-11 22:46:37 +0800

    Add msw setup for Vitest

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,11 +1,39 @@
-import { expect, afterEach } from "vitest"
+import { expect, beforeAll, afterEach, afterAll } from "vitest"
 import { cleanup } from "@testing-library/react"
 import matchers from "@testing-library/jest-dom/matchers"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
+import {nanoid} from "nanoid"
+
+function getTransaction(name, amount, transactionDate) {
+  return {
+    id: nanoid(),
+    name,
+    amount,
+    transactionDate
+  }
+}
+
+export const restHandlers = [
+    rest.post("/api/wallet/transaction/create", async (req, res, ctx) => {
+      const { name, amount, transactionDate } = await req.json()
+      const transaction = getTransaction(name, amount, transactionDate)
+      return res(ctx.status(200), ctx.json(transaction))
+    })
+]
+
+const server = setupServer(...restHandlers)
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
 
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }))
+
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup()
+  // Reset handlers after each test `important for test isolation`
+  server.resetHandlers()
 })
+
+afterAll(() => server.close())
