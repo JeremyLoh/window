9083635073cd11283710d27b00f9ba1347b465aa
JeremyLoh <jeremyloh03@gmail.com>
2022-12-18 23:15:11 +0800

    Add total expense calculation for transactions

--- components/wallet/walletSummary.tsx
+++ components/wallet/walletSummary.tsx
@@ -1,17 +1,29 @@
-import React from "react"
+import React, { useContext } from "react"
 import CardInfo from "../../components/cardInfo"
 import styles from "../../styles/components/wallet/WalletSummary.module.css"
+import { Transaction, TransactionContext } from "../../pages/wallet"
 
 export function WalletSummary({}) {
+  const transactions: Array<Transaction> = useContext(TransactionContext)
+
+  function getTotalExpense(): string {
+    const total: number = transactions
+      .filter((transaction) => transaction.isExpense)
+      .reduce((accumulator, transaction) =>
+        accumulator + transaction.amount
+      , 0)
+    return total.toFixed(2)
+  }
+
   return (
     <div className={styles.walletSummary}>
       <CardInfo ariaLabel="wallet-expenses">
         <h2>Expenses</h2>
-        <p className={styles.warning}>$0</p>
+        <p className={styles.warning}>${getTotalExpense()}</p>
       </CardInfo>
       <CardInfo ariaLabel="wallet-income">
         <h2>Income</h2>
-        <p>$0</p>
+        <p>$0.00</p>
       </CardInfo>
     </div>
   )
