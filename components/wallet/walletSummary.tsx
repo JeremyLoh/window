fcec8b27d9bce4ce46be6b2f2c66ab64f4e2f8e3
JeremyLoh <jeremyloh03@gmail.com>
2022-12-24 22:05:30 +0800

    Add cash flow display for selected date

--- components/wallet/walletSummary.tsx
+++ components/wallet/walletSummary.tsx
@@ -5,6 +5,9 @@ import { Transaction, TransactionContext } from "../../pages/wallet"
 
 export function WalletSummary({}) {
   const transactions: Array<Transaction> = useContext(TransactionContext)
+  const totalExpense: string = getTotalExpense()
+  const totalIncome: string = getTotalIncome()
+  const cashFlow: string = getCashFlow()
 
   function getTotalAmount(transactions: Array<Transaction>): number {
     return transactions.reduce((accumulator, transaction) =>
@@ -23,6 +26,14 @@ export function WalletSummary({}) {
     return total.toFixed(2)
   }
 
+  function getCashFlow(): string {
+    const cashFlow: string = Math.abs(Number(totalIncome) - Number(totalExpense)).toFixed(2)
+    if (Number(totalIncome) < Number(totalExpense)) {
+      return "-$" + cashFlow
+    }
+    return "$" + cashFlow
+  }
+
   return (
     <div>
       <h1 aria-label="wallet-transaction-day-count">
@@ -32,11 +43,15 @@ export function WalletSummary({}) {
       <div className={styles.walletSummary}>
         <CardInfo ariaLabel="wallet-expenses">
           <h2>Expenses</h2>
-          <p className={styles.warning}>${getTotalExpense()}</p>
+          <p className={styles.warning}>${totalExpense}</p>
         </CardInfo>
         <CardInfo ariaLabel="wallet-income">
           <h2>Income</h2>
-          <p>${getTotalIncome()}</p>
+          <p>${totalIncome}</p>
+        </CardInfo>
+        <CardInfo ariaLabel="wallet-cash-flow">
+          <h2>Cash Flow</h2>
+          <p>{cashFlow}</p>
         </CardInfo>
       </div>
     </div>
