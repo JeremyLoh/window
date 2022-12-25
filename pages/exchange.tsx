c6e3bf5a47ca136449ebea067ea135ebf9273d13
JeremyLoh <jeremyloh03@gmail.com>
2022-12-25 15:51:45 +0800

    Create exchange rate page

---
+++ pages/exchange.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function Exchange() {
+  return (
+    <div>
+      <Head>
+        <title>Window</title>
+        <meta name="description" content="Exchange Rate" />
+      </Head>
+    </div>
+  )
+}
