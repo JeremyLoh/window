025d9067d6ab0e43cec5ea76d1077c896d2bdde7
JeremyLoh <jeremyloh03@gmail.com>
2022-12-27 21:09:31 +0800

    Add style for exchange form

--- pages/exchange.tsx
+++ pages/exchange.tsx
@@ -2,7 +2,9 @@ import Head from "next/head"
 import React, { useState } from "react"
 import axios from "axios"
 import Currency from "../components/currency"
+import Emoji from "../components/emoji"
 import { RequestData } from "./api/exchange"
+import styles from "../styles/pages/Exchange.module.css"
 
 export default function Exchange() {
   const [amount, setAmount] = useState<Currency>(new Currency(0))
@@ -25,46 +27,66 @@ export default function Exchange() {
   }
 
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Window</title>
         <meta name="description" content="Exchange Rate" />
       </Head>
 
+      <h1 className={styles.pageTitle}>
+        Exchange {" "}
+        <Emoji symbol="🌎" label="world" />
+      </h1>
+
       <form action="/api/exchange"
             method="post"
             onSubmit={handleSubmit}
+            className={styles.currencyForm}
       >
-        <label htmlFor="conversion-amount">Conversion Amount</label>
-        <input type="number"
-               required
-               id="conversion-amount"
-               min="0.01"
-               max="99999999"
-               step="0.01"
-               value={String(amount.getAmountInDollars())}
-               onChange={handleChangeAmount}
-        />
-
-        <label htmlFor="from-currency-select">From</label>
-        <select required defaultValue="" aria-label="from-currency-select">
-          <option value="" aria-label="default-from-currency" disabled hidden>
-            -- select an option --
-          </option>
-          <option value="SGD" aria-label="from-SGD">SGD - Singapore Dollar</option>
-          <option value="USD" aria-label="from-USD">USD - United States Dollar</option>
-        </select>
-
-        <label htmlFor="to-currency-select">To</label>
-        <select required defaultValue="" aria-label="to-currency-select">
-          <option value="" aria-label="default-to-currency" disabled hidden>
-            -- select an option --
-          </option>
-          <option value="SGD" aria-label="to-SGD">SGD - Singapore Dollar</option>
-          <option value="USD" aria-label="to-USD">USD - United States Dollar</option>
-        </select>
-
-        <button type="submit">Submit</button>
+        <div className={styles.currencyFormInput}>
+          <label htmlFor="conversion-amount">Conversion Amount</label>
+          <input type="number"
+                 required
+                 id="conversion-amount"
+                 min="0.01"
+                 max="99999999"
+                 step="0.01"
+                 value={String(amount.getAmountInDollars())}
+                 onChange={handleChangeAmount}
+                 className={styles.input}
+          />
+        </div>
+        <div className={styles.currencyFormInput}>
+          <label htmlFor="from-currency-select">From</label>
+          <select required
+                  defaultValue=""
+                  aria-label="from-currency-select"
+                  className={styles.input}
+          >
+            <option value="" aria-label="default-from-currency" disabled hidden>
+              -- select an option --
+            </option>
+            <option value="SGD" aria-label="from-SGD">SGD - Singapore Dollar</option>
+            <option value="USD" aria-label="from-USD">USD - United States Dollar</option>
+          </select>
+        </div>
+        <div className={styles.currencyFormInput}>
+          <label htmlFor="to-currency-select">To</label>
+          <select required
+                  defaultValue=""
+                  aria-label="to-currency-select"
+                  className={styles.input}
+          >
+            <option value="" aria-label="default-to-currency" disabled hidden>
+              -- select an option --
+            </option>
+            <option value="SGD" aria-label="to-SGD">SGD - Singapore Dollar</option>
+            <option value="USD" aria-label="to-USD">USD - United States Dollar</option>
+          </select>
+        </div>
+        <button type="submit" className={styles.currencyFormButton}>
+          Submit
+        </button>
       </form>
     </div>
   )
