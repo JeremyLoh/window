f7ee861613af9dae941b305f4351cd54748f5483
JeremyLoh <jeremyloh03@gmail.com>
2023-01-14 19:51:37 +0800

    Add economy display to exchange page

--- pages/exchange.tsx
+++ pages/exchange.tsx
@@ -4,12 +4,22 @@ import Emoji from "../components/emoji"
 import { getCurrencySymbols, Symbol } from "../lib/exchange/currency/symbols"
 import ExchangeRateDisplay from "../components/exchange/exchangeRateDisplay"
 import styles from "../styles/pages/Exchange.module.css"
+import EconomyDisplay, { Country } from "../components/exchange/economyDisplay"
 
 type ExchangeProps = {
   symbols:  Record<string, Symbol>
 }
 
 export default function Exchange(props: ExchangeProps) {
+  // TODO make api call to get countries
+  const countries: Map<string, Country> = new Map([
+    ["Singapore", {
+      alpha2Code: "sg",
+      name: "Singapore",
+      flag: "\ud83c\uddf8\ud83c\uddec",
+    }]
+  ])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,6 +33,8 @@ export default function Exchange(props: ExchangeProps) {
       </h1>
 
       <ExchangeRateDisplay symbols={props.symbols} />
+
+      <EconomyDisplay countries={countries} />
     </div>
   )
 }
