59715e37e46c7eb85a3ebd2b08a30ddd4533b0a1
JeremyLoh <jeremyloh03@gmail.com>
2023-01-15 23:24:34 +0800

    Filter country for economy data

--- lib/exchange/economy/country.ts
+++ lib/exchange/economy/country.ts
@@ -20,7 +20,7 @@ export async function getCountries(): Promise<Array<Country>> {
   if (response.status !== 200) {
     throw new Error("External Rest Countries API is down")
   }
-  const data: RestCountryResponse = response.data
+  const data: RestCountryResponse = getAvailableCountryCodes(response.data)
   TimSort.sort(data, (a: CountryDetails, b: CountryDetails) =>
     a.name.common.localeCompare(b.name.common))
   return data.map((country: CountryDetails) => {
@@ -31,4 +31,38 @@ export async function getCountries(): Promise<Array<Country>> {
       flag,
     }
   })
+}
+
+const availableCountryData: [string, string][] = [
+  ["AL", "Albania"], ["DZ", "Algeria"], ["AO", "Angola"], ["AR", "Argentina"], ["AU", "Australia"],
+  ["AT", "Austria"], ["AZ", "Azerbaijan"], ["BD", "Bangladesh"], ["BY", "Belarus"],
+  ["BE", "Belgium"], ["BO", "Bolivia"], ["BA", "Bosnia And Herzegovina"], ["BR", "Brazil"],
+  ["BG", "Bulgaria"], ["KH", "Cambodia"], ["CA", "Canada"], ["CL", "Chile"], ["CN", "China"],
+  ["CO", "Colombia"], ["CR", "Costa Rica"], ["HR", "Croatia"], ["CY", "Cyprus"], ["CZ", "Czechia"],
+  ["CD", "Democratic Republic Of Congo"], ["DK", "Denmark"], ["DO", "Dominican Republic"],
+  ["EC", "Ecuador"], ["EG", "Egypt"], ["SV", "El Salvador"], ["EE", "Estonia"], ["ET", "Ethiopia"],
+  ["EU", "European Union"], ["FI", "Finland"], ["FR", "France"], ["DE", "Germany"], ["GH", "Ghana"],
+  ["GR", "Greece"], ["GT", "Guatemala"], ["HN", "Honduras"], ["HK", "Hong Kong"], ["HU", "Hungary"],
+  ["IN", "India"], ["ID", "Indonesia"], ["IR", "Iran"], ["IQ", "Iraq"], ["IE", "Ireland"],
+  ["IL", "Israel"], ["IT", "Italy"], ["JP", "Japan"], ["JO", "Jordan"], ["KZ", "Kazakhstan"],
+  ["KE", "Kenya"], ["KW", "Kuwait"], ["KG", "Kyrgyzstan"], ["LA", "Laos"], ["LV", "Latvia"],
+  ["LB", "Lebanon"], ["LY", "Libya"], ["LT", "Lithuania"], ["LU", "Luxembourg"], ["MO", "Macao"],
+  ["MY", "Malaysia"], ["MX", "Mexico"], ["MN", "Mongolia"], ["MA", "Morocco"], ["MM", "Myanmar"],
+  ["NP", "Nepal"], ["NL", "Netherlands"], ["NZ", "New Zealand"], ["NI", "Nicaragua"],
+  ["NG", "Nigeria"], ["NO", "Norway"], ["OM", "Oman"], ["PK", "Pakistan"], ["PA", "Panama"],
+  ["PY", "Paraguay"], ["PE", "Peru"], ["PH", "Philippines"], ["PL", "Poland"], ["PT", "Portugal"],
+  ["QA", "Qatar"], ["RO", "Romania"], ["RU", "Russian Federation"], ["SA", "Saudi Arabia"],
+  ["SN", "Senegal"], ["RS", "Serbia"], ["SG", "Singapore"], ["SK", "Slovakia"], ["SI", "Slovenia"],
+  ["ZA", "South Africa"], ["KR", "South Korea"], ["ES", "Spain"], ["LK", "Sri Lanka"],
+  ["SD", "Sudan"], ["SE", "Sweden"], ["CH", "Switzerland"], ["TW", "Taiwan"], ["TJ", "Tajikistan"],
+  ["TZ", "Tanzania"], ["TH", "Thailand"], ["TN", "Tunisia"], ["TR", "Turkey"],
+  ["TM", "Turkmenistan"], ["UA", "Ukraine"], ["AE", "United Arab Emirates"],
+  ["UK", "United Kingdom"], ["US", "United States"], ["UY", "Uruguay"], ["UZ", "Uzbekistan"],
+  ["VE", "Venezuela"], ["VN", "Vietnam"],
+]
+
+function getAvailableCountryCodes(data: Array<CountryDetails>): Array<CountryDetails> {
+  // https://www.econdb.com/docs/clients/prognosis#available-country-codes
+  const codes = new Map(availableCountryData)
+  return data.filter((country: CountryDetails) => codes.has(country.cca2))
 }
