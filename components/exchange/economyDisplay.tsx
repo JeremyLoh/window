e72361ffda7a983a8bcc118b6de162e93a1187da
JeremyLoh <jeremyloh03@gmail.com>
2023-01-22 01:22:01 +0800

    Add line chart for CPI data display

--- components/exchange/economyDisplay.tsx
+++ components/exchange/economyDisplay.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useEffect, useState } from "react"
 import CardInfo from "../cardInfo"
+import { Cpi, getCPI } from "../../lib/exchange/economy/indicators"
+import LineChart from "../chart/lineChart"
 import styles from "../../styles/components/exchange/EconomyDisplay.module.css"
 
 export type Country = {
@@ -13,8 +15,9 @@ export type EconomyDisplayProps = {
 }
 
 const EconomyDisplay:FC<EconomyDisplayProps> = (props) => {
-  const [country, setCountry] = useState<string>("")
+  const [countryAlphaTwoCode, setCountryAlphaTwoCode] = useState<string>("")
   const [countryOptions, setCountryOptions] = useState<JSX.Element[]>()
+  const [cpiData, setCpiData] = useState<Cpi>()
 
   useEffect(() => {
     function getCountryInputOptions(): JSX.Element[] {
@@ -35,40 +38,55 @@ const EconomyDisplay:FC<EconomyDisplayProps> = (props) => {
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    // TODO retrieve data from econdb api based on country alpha2Code
+    const cpi: Cpi = await getCPI(countryAlphaTwoCode)
+    // todo check if "data" of response.data is { dates: [], values: [], status: [] }, it is not available
+
+    setCpiData(cpi)
   }
 
   function handleCountryChange(event: React.ChangeEvent<HTMLSelectElement>): void {
-    setCountry(event.target.value)
+    setCountryAlphaTwoCode(event.target.value)
   }
 
   return (
-    <CardInfo>
-      <h2>View Economic Data</h2>
-      <form action="/api/exchange/economy/country"
-            method="post"
-            onSubmit={handleSubmit}
-            aria-label="exchange-economy-form"
-            className={styles.economyCountryForm}
-      >
-        <div className={styles.countryInput}>
-          <label htmlFor="economy-country-select">Country</label>
-          <select required
-                  aria-label="economy-country-select"
-                  value={country}
-                  onChange={handleCountryChange}
-          >
-            <option value="" aria-label="default-country" disabled hidden>
-              -- select a country --
-            </option>
-            { props.countries && countryOptions }
-          </select>
-        </div>
-        <button type="submit">
-          Get Economic Data
-        </button>
-      </form>
-    </CardInfo>
+    <div style={{display: "flex", flexDirection: "column", width: "80%"}}>
+      <CardInfo>
+        <h2>View Economic Data</h2>
+        <form action="/api/exchange/economy/country"
+              method="post"
+              onSubmit={handleSubmit}
+              aria-label="exchange-economy-form"
+              className={styles.economyCountryForm}
+        >
+          <div className={styles.countryInput}>
+            <label htmlFor="economy-country-select">Country</label>
+            <select required
+                    aria-label="economy-country-select"
+                    value={countryAlphaTwoCode}
+                    onChange={handleCountryChange}
+            >
+              <option value="" aria-label="default-country" disabled hidden>
+                -- select a country --
+              </option>
+              { props.countries && countryOptions }
+            </select>
+          </div>
+          <button type="submit">
+            Get Economic Data
+          </button>
+        </form>
+      </CardInfo>
+      {
+        cpiData && (
+          <div style={{position: "relative", width: "100%", height: "100%"}}>
+            <LineChart ariaLabel="cpi-graph"
+                       title={`${cpiData.ticker} - ${cpiData.description}`}
+                       description={`${cpiData.description}`}
+                       xLabels={cpiData.dates}
+                       yLabelData={cpiData.values} />
+        </div>)
+      }
+    </div>
   )
 }
 
