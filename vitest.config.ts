cfd5ceb7f79be8bd96924a213c1083609910483d
JeremyLoh <jeremyloh03@gmail.com>
2023-01-22 11:57:37 +0800

    Update test setup to mock canvas and next/image

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,9 +7,6 @@ export default defineConfig({
   test: {
     setupFiles: ["./__tests__/setup.ts"],
     environment: "jsdom",
-    deps: {
-      inline: ["./node_modules/vitest-canvas-mock/dist/index.js"],
-    },
     threads: false, // fix canvas error, https://github.com/vitest-dev/vitest/issues/740
     environmentOptions: {
       jsdom: {
