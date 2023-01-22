20322cab27da37c3da57408031c48b43872ba5b4
JeremyLoh <jeremyloh03@gmail.com>
2023-01-22 20:34:02 +0800

    Add multi-select component for series data

--- components/exchange/economyCountryForm.tsx
+++ components/exchange/economyCountryForm.tsx
@@ -1,7 +1,8 @@
 import React, { FC, useEffect, useState } from "react"
 import CardInfo from "../cardInfo"
-import { Cpi, getCPI } from "../../lib/exchange/economy/indicators"
+import { Cpi, getCPI, getCountrySeries } from "../../lib/exchange/economy/indicators"
 import { Country } from "./economyDisplay"
+import Select, { ActionMeta, GroupBase, StylesConfig } from "react-select"
 import styles from "../../styles/components/exchange/EconomyCountryForm.module.css"
 
 type EconomyCountryFormProps = {
@@ -9,9 +10,38 @@ type EconomyCountryFormProps = {
   countries: Map<string, Country>,
 }
 
+type Option = {
+  key: string,
+  label: string,
+  value: string,
+}
+
+const multiSelectStyles: StylesConfig<Option, true, GroupBase<Option>> = {
+  option: (provided, state) => {
+    return {
+      ...provided,
+      color: "black",
+    }
+  },
+  control: (provided, state) => {
+    return {
+      ...provided,
+      color: "black",
+    }
+  },
+  singleValue: (provided, state) => {
+    return {
+      ...provided,
+      color: "black",
+    }
+  }
+}
+
 const EconomyCountryForm:FC<EconomyCountryFormProps> = (props) => {
-  const [countryAlphaTwoCode, setCountryAlphaTwoCode] = useState<string>("")
+  const [countryName, setCountryName] = useState<string>("")
   const [countryOptions, setCountryOptions] = useState<JSX.Element[]>()
+  const [series, setSeries] = useState<Array<Option>>([])
+  const [selectedSeries, setSelectedSeries] = useState<Array<Option>>([])
 
   useEffect(() => {
     function getCountryInputOptions(): JSX.Element[] {
@@ -19,7 +49,7 @@ const EconomyCountryForm:FC<EconomyCountryFormProps> = (props) => {
       return countries.map((country) => {
         return (
           <option key={country.alpha2Code}
-                  value={country.alpha2Code}
+                  value={country.name}
                   aria-label={`economy-country-${country.name}`}
           >
             {country.name}
@@ -32,12 +62,27 @@ const EconomyCountryForm:FC<EconomyCountryFormProps> = (props) => {
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    const cpi: Cpi = await getCPI(countryAlphaTwoCode)
+    const country: Country | undefined = props.countries.get(countryName)
+    if (country == null) {
+      return
+    }
+    const cpi: Cpi = await getCPI(country.alpha2Code)
     props.handleSubmit(cpi)
   }
 
-  function handleCountryChange(event: React.ChangeEvent<HTMLSelectElement>): void {
-    setCountryAlphaTwoCode(event.target.value)
+  async function handleCountryChange(event: React.ChangeEvent<HTMLSelectElement>): Promise<void> {
+    const countryName = event.target.value
+    const country: Country | undefined = props.countries.get(countryName)
+    if (country == null) {
+      return
+    }
+    setCountryName(countryName)
+    // todo update list of series available, cache results of query
+    // const data = await getCountrySeries(country.name)
+  }
+
+  function handleSeriesChange(option: readonly Option[], actionMeta: ActionMeta<Option>): void {
+    setSelectedSeries(option.map((o) => o))
   }
 
   return (
@@ -49,11 +94,11 @@ const EconomyCountryForm:FC<EconomyCountryFormProps> = (props) => {
             aria-label="exchange-economy-form"
             className={styles.economyCountryForm}
       >
-        <div className={styles.countryInput}>
+        <div className={styles.dropdownContainer}>
           <label htmlFor="economy-country-select">Country</label>
           <select required
                   aria-label="economy-country-select"
-                  value={countryAlphaTwoCode}
+                  value={countryName}
                   onChange={handleCountryChange}
           >
             <option value="" aria-label="default-country" disabled hidden>
@@ -62,7 +107,23 @@ const EconomyCountryForm:FC<EconomyCountryFormProps> = (props) => {
             { props.countries && countryOptions }
           </select>
         </div>
-        <button type="submit">
+        {
+          (series.length > 0) && (
+            <div className={styles.dropdownContainer}>
+              <label htmlFor="economy-country-series">Series</label>
+              <Select aria-label="economy-country-series"
+                      name="economy-country-series"
+                      required
+                      isMulti
+                      options={series}
+                      value={selectedSeries}
+                      onChange={handleSeriesChange}
+                      styles={multiSelectStyles}
+              />
+            </div>
+          )
+        }
+        <button type="submit" className={styles.submitButton}>
           Get Economic Data
         </button>
       </form>
