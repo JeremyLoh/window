3a45d8b7eab1d3b366473720a5a5ca44948c694f
JeremyLoh <jeremyloh03@gmail.com>
2023-01-23 19:01:04 +0800

    Remove unused CPI data function

--- lib/exchange/economy/indicators.ts
+++ lib/exchange/economy/indicators.ts
@@ -35,45 +35,6 @@ export async function getCountrySeries(country: string): Promise<Array<Series>> 
   return series
 }
 
-type CpiResponse = {
-  ticker: string,
-  description: string,
-  geography: string,
-  frequency: string,
-  dataset: string,
-  units: string,
-  data: {
-    values: Array<number>,
-    dates: Array<string>,
-  }
-}
-
-export type Cpi = {
-  ticker: string,
-  description: string,
-  dates: Array<string>,
-  values: Array<number>,
-}
-
-export async function getCPI(countryAlphaTwoCode: string): Promise<Cpi> {
-  const url: string = `https://www.econdb.com/api/series/CPI${countryAlphaTwoCode}/?format=json`
-  const response: AxiosResponse = await axios.get(url)
-  if (response.status !== 200) {
-    throw new Error(`Could not get CPI data for ${countryAlphaTwoCode}`)
-  }
-  const cpiData: CpiResponse = response.data
-  return extractCpiData(cpiData)
-}
-
-function extractCpiData(cpiData: CpiResponse): Cpi {
-  return {
-    ticker: cpiData.ticker,
-    description: cpiData.description,
-    dates: cpiData.data.dates,
-    values: cpiData.data.values,
-  }
-}
-
 export type EconomySeries = {
   ticker: string,
   description: string,
