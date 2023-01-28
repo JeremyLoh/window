ec1e1e3166ee381a90add8199bc2ec15ed54e9ed
JeremyLoh <jeremyloh03@gmail.com>
2023-01-28 16:45:08 +0800

    Move footer to show on all pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head"
-import Image from "next/image"
 import Emoji from "../components/emoji"
 import Clock from "../components/clock"
 import CardLink from "../components/cardLink"
-import profilePicture from "../public/profilePicture.jpg"
 
 export default function Home() {
   return (
@@ -38,24 +36,6 @@ export default function Home() {
           </section>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center border-t border-zinc-400 py-4">
-        <a
-          className="flex flex-grow items-center justify-center gap-x-3"
-          href="https://github.com/JeremyLoh/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made by
-          <Image
-            className="rounded-full"
-            src={profilePicture}
-            alt="Jeremy Loh Profile Picture"
-            width={88}
-            height={88}
-          />
-        </a>
-      </footer>
     </div>
   )
 }
