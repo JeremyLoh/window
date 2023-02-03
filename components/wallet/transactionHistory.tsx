c51fe036d93f64c403d54d0bcb52ca7a96e0a50e
JeremyLoh <jeremyloh03@gmail.com>
2023-02-03 23:39:01 +0800

    Update Wallet transaction history CSS

--- components/wallet/transactionHistory.tsx
+++ components/wallet/transactionHistory.tsx
@@ -1,9 +1,8 @@
 import React, { FC, useContext, useState } from "react"
 import TransactionInfo from "./transactionInfo"
-import styles from "../../styles/components/wallet/TransactionHistory.module.css"
 import { Transaction, TransactionContext } from "../../pages/wallet"
 
-const TransactionHistory:FC<any> = () => {
+const TransactionHistory: FC<any> = () => {
   const [pageIndex, setPageIndex] = useState<number>(1)
   const transactions: Array<Transaction> = useContext(TransactionContext)
 
@@ -13,42 +12,47 @@ const TransactionHistory:FC<any> = () => {
     const end = pageIndex * transactionsPerPage
     return (
       <div aria-label="transactions-container">
-        {
-          transactions.slice(start, end).map((transaction: Transaction) => {
-            return (
-              <TransactionInfo key={transaction.id}
-                transaction={transaction}
-              />
-            )
-          })
-        }
+        {transactions.slice(start, end).map((transaction: Transaction) => {
+          return (
+            <TransactionInfo key={transaction.id} transaction={transaction} />
+          )
+        })}
       </div>
     )
   }
 
   return (
-    <div aria-label="wallet-transaction-history" className={styles.container}>
-      <div className={styles.pagination}>
-        <button aria-label="previous-transaction-history"
-                disabled={pageIndex === 1}
-                onClick={() => setPageIndex(pageIndex - 1)}
+    <div
+      aria-label="wallet-transaction-history"
+      className="flex w-full flex-col justify-start"
+    >
+      <div className="flex flex-row gap-4 pb-4">
+        <button
+          aria-label="previous-transaction-history"
+          className="rounded bg-white px-2 text-black disabled:cursor-not-allowed"
+          disabled={pageIndex === 1}
+          onClick={() => setPageIndex(pageIndex - 1)}
         >
           Previous
         </button>
-        <button aria-label="current-transaction-history-page">
+        <button
+          aria-label="current-transaction-history-page"
+          className="rounded bg-purple-300 px-4 text-lg text-black"
+          disabled
+        >
           {pageIndex}
         </button>
-        <button aria-label="next-transaction-history"
-                disabled={(pageIndex * 10) >= transactions.length}
-                onClick={() => setPageIndex(pageIndex + 1)}
+        <button
+          aria-label="next-transaction-history"
+          className="rounded bg-white px-2 text-black disabled:cursor-not-allowed"
+          disabled={pageIndex * 10 >= transactions.length}
+          onClick={() => setPageIndex(pageIndex + 1)}
         >
           Next
         </button>
       </div>
 
-      { transactions &&
-        getPaginatedTransactions(transactions)
-      }
+      {transactions && getPaginatedTransactions(transactions)}
     </div>
   )
 }
