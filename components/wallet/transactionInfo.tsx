00589526699d8d35461325af656d2d3997e2861d
JeremyLoh <jeremyloh03@gmail.com>
2023-02-04 01:24:41 +0800

    Update wallet transaction info CSS

--- components/wallet/transactionInfo.tsx
+++ components/wallet/transactionInfo.tsx
@@ -1,33 +1,47 @@
 import React, { FC, useContext } from "react"
-import styles from "../../styles/components/wallet/TransactionInfo.module.css"
 import {
   Transaction,
   DeleteTransactionContext,
-  DeleteTransactionType
+  DeleteTransactionType,
 } from "../../pages/wallet"
 
 interface TransactionInfoProps {
   transaction: Transaction
 }
 
-const TransactionInfo:FC<TransactionInfoProps> = (props) => {
-  const deleteTransaction: DeleteTransactionType = useContext(DeleteTransactionContext)
-  const transactionTypeStyle: string = props.transaction.isExpense ? styles.expense
-    : styles.income
+const TransactionInfo: FC<TransactionInfoProps> = (props) => {
+  const deleteTransaction: DeleteTransactionType = useContext(
+    DeleteTransactionContext
+  )
+  const transactionTypeStyle: string = props.transaction.isExpense
+    ? "bg-red-700 break-all"
+    : "bg-green-700 break-all"
+  const containerStyle =
+    "mb-4 flex flex-col items-start justify-center rounded-2xl py-2 px-6 last:mb-0 sm:flex-row md:items-center md:gap-3"
 
   return (
-    <div className={`${styles.container} ${transactionTypeStyle}`}
-         aria-label="transaction"
+    <div
+      className={`${containerStyle} ${transactionTypeStyle}`}
+      aria-label="transaction"
     >
-      <h1 className={styles.name}>{props.transaction.name}</h1>
-      <div className={styles.info}>
-        <h2>{props.transaction.amount.format()}</h2>
-        <h3>{props.transaction.transactionDate.toLocaleDateString()}</h3>
+      <h1 className="mb-4 flex-grow break-all">{props.transaction.name}</h1>
+      <div className="flex w-full  flex-col items-end md:w-auto md:items-center">
+        <h2 className="w-full rounded bg-white py-2 text-center text-black">
+          {props.transaction.amount.format()}
+        </h2>
+        <h3 className="rounded bg-gray-300 py-1 px-4 text-black">
+          {props.transaction.transactionDate.toLocaleDateString()}
+        </h3>
       </div>
-      <button className={styles.deleteBtn}
-              aria-label="delete-transaction"
-              onClick={() => deleteTransaction(props.transaction.id,
-                props.transaction.transactionDate)}
+      <button
+        className="inline-flex aspect-square items-center rounded-full border-2 border-gray-500 bg-gray-800 p-3 text-lg hover:bg-gray-600"
+        aria-label="delete-transaction"
+        onClick={() =>
+          deleteTransaction(
+            props.transaction.id,
+            props.transaction.transactionDate
+          )
+        }
       >
         X
       </button>
