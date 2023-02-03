6d1a0fd085a95af4e14ded48c181bd03455fc4e4
JeremyLoh <jeremyloh03@gmail.com>
2023-02-04 01:41:23 +0800

    Update wallet history colors and margin

--- components/wallet/transactionHistory.tsx
+++ components/wallet/transactionHistory.tsx
@@ -29,7 +29,7 @@ const TransactionHistory: FC<any> = () => {
       <div className="flex flex-row gap-4 pb-4">
         <button
           aria-label="previous-transaction-history"
-          className="rounded bg-white px-2 text-black disabled:cursor-not-allowed"
+          className="rounded bg-cyan-400 px-2 text-black disabled:cursor-not-allowed"
           disabled={pageIndex === 1}
           onClick={() => setPageIndex(pageIndex - 1)}
         >
@@ -37,14 +37,14 @@ const TransactionHistory: FC<any> = () => {
         </button>
         <button
           aria-label="current-transaction-history-page"
-          className="rounded bg-purple-300 px-4 text-lg text-black"
+          className="rounded bg-cyan-500 px-4 text-lg text-black"
           disabled
         >
           {pageIndex}
         </button>
         <button
           aria-label="next-transaction-history"
-          className="rounded bg-white px-2 text-black disabled:cursor-not-allowed"
+          className="rounded bg-cyan-400 px-2 text-black disabled:cursor-not-allowed"
           disabled={pageIndex * 10 >= transactions.length}
           onClick={() => setPageIndex(pageIndex + 1)}
         >
