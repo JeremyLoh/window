49a02af55b2e1bf786053399e2f21c254d5a0fa3
JeremyLoh <jeremyloh03@gmail.com>
2023-02-04 12:19:24 +0800

    Update background for wallet add transaction form

--- components/wallet/walletAddTransactionForm.tsx
+++ components/wallet/walletAddTransactionForm.tsx
@@ -95,7 +95,7 @@ const WalletForm: FC<WalletFormProps> = (props) => {
         name="transactionDate"
       />
       <button
-        className="rounded border-b-4 border-blue-700 bg-blue-500 py-2 px-4 font-bold text-white hover:border-blue-500 hover:bg-blue-400"
+        className="rounded border-b-4 border-cyan-700 bg-cyan-500 py-2 px-4 font-bold text-white hover:border-cyan-500 hover:bg-cyan-400"
         type="submit"
       >
         Submit
