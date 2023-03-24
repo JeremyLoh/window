eee7c152eee5a888f5981c0eddb2216389dabba1
JeremyLoh <jeremyloh03@gmail.com>
2023-03-24 22:42:55 +0800

    Fix compile error due to cypress command file

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,8 @@
+// To remove Type error: 'commands.ts' cannot be compiled under '--isolatedModules'
+// because it is considered a global script file.
+// Add an import, export, or an empty 'export {}' statement to make it a module.
+export {}
+
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
