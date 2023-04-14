57c2d43b7525264950fcde58871363750e204fa8
JeremyLoh <jeremyloh03@gmail.com>
2023-04-14 23:46:51 +0800

    Add life page and tests for birthday input

--- cypress/e2e/life/index.cy.tsx
+++ cypress/e2e/life/index.cy.tsx
@@ -1,3 +1,5 @@
+import format from "date-fns/format"
+
 describe("life", () => {
   beforeEach(() => {
     cy.visit("/life")
@@ -10,5 +12,59 @@ describe("life", () => {
   it("should show quote", () => {
     cy.getByTestId("life-quote").should("be.visible")
       .should("have.length.greaterThan", 0)
+  })
+
+  context("date of birth input", () => {
+    function getDateOfBirthForm() {
+      return cy.getByTestId("life-date-of-birth")
+    }
+
+    function getDateOfBirthInput() {
+      return getDateOfBirthForm().find("input[type='date']")
+    }
+
+    function formatDateOfBirthInput(date: Date) {
+      return format(date, "yyyy-LL-dd")
+    }
+
+    it("should show date of birth label", () => {
+      getDateOfBirthForm().find("label")
+        .should("be.visible")
+        .and("contain.text", "Enter your date of birth")
+    })
+
+    it("should show date of birth input", () => {
+      const currentDate: Date = new Date()
+      const expectedMinYear: number = currentDate.getFullYear() - 80
+      getDateOfBirthInput().invoke("attr", "min")
+        .should("eq", `${expectedMinYear}-01-01`)
+
+      const expectedMaxDate = formatDateOfBirthInput(currentDate)
+      getDateOfBirthInput().invoke("attr", "max")
+        .should("eq", expectedMaxDate)
+    })
+
+    it("should allow date of birth input change", () => {
+      getDateOfBirthInput().type("1999-12-31")
+      getDateOfBirthInput().invoke("val")
+        .should("eq", "1999-12-31")
+      getDateOfBirthInput().invoke("prop", "validationMessage")
+        .should("equal", "")
+    })
+
+    it("should give warning for date of birth of tomorrow", () => {
+      const tomorrow: Date = new Date()
+      tomorrow.setDate(tomorrow.getDate() + 1)
+      getDateOfBirthInput().type(formatDateOfBirthInput(tomorrow))
+
+      const today: string = formatDateOfBirthInput(new Date())
+      const expectedValidationMessage = `Please select a value that is no later than ${today}.`
+      getDateOfBirthInput().invoke("prop", "validationMessage")
+        .should("equal", expectedValidationMessage)
+      getDateOfBirthInput().should("have.css", "border-color",
+        "rgb(236, 72, 153)")
+      getDateOfBirthInput().should("have.css", "color",
+        "rgb(220, 38, 38)")
+    })
   })
 })
--- pages/life.tsx
+++ pages/life.tsx
@@ -1,8 +1,29 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import Head from "next/head"
+import format from "date-fns/format"
 import Navbar from "../components/navbar"
+import Quote from "../components/life/quote"
 
 const Life: FC<any> = () => {
+  const [dateOfBirth, setDateOfBirth] = useState<Date>(new Date())
+  const currentDate = new Date()
+  const minYear = currentDate.getFullYear() - 80
+
+  function formatDate(date: Date) {
+    // e.g. 25th November 2022 => "yyyy-LL-dd" => 2022-11-25
+    return format(date, "yyyy-LL-dd")
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    // TODO generate life calendar
+    return false
+  }
+
+  function handleDateChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setDateOfBirth(new Date(event.target.value))
+  }
+
   return (
     <div className="min-h-screen">
       <Head>
@@ -11,21 +32,40 @@ const Life: FC<any> = () => {
       </Head>
 
       <Navbar />
-      <blockquote data-test="life-quote" className="text-center pt-2 bg-gray-700 rounded-lg mt-4 w-4/5 lg:w-2/5 mx-auto">
-        <svg aria-hidden="true"
-             className="w-10 h-10 text-gray-400 dark:text-white ml-5"
-             viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
-            fill="currentColor" />
-        </svg>
+      <Quote dataTest="life-quote">
         <p className="text-lg -mt-2 p-2 sm:-mt-10">
           A small night storm blows <br />
           Saying “Falling is the essence of a flower” <br />
           Preceding those who hesitate.
         </p>
         <p>— Yukio Mishima</p>
-      </blockquote>
+      </Quote>
+      <form data-test="life-date-of-birth"
+            className="mt-4 flex flex-col lg:flex-row justify-center items-center"
+            onSubmit={handleSubmit}
+      >
+        <label className="lg:text-lg p-2" htmlFor="birthDate">
+          Enter your date of birth
+        </label>
+        <input required
+               className="text-black rounded-md border shadow-sm focus:ring
+                          invalid:border-pink-500 invalid:text-red-600"
+               id="birthDate"
+               name="birthDate"
+               type="date"
+               min={`${minYear}-01-01`}
+               max={formatDate(currentDate)}
+               value={formatDate(dateOfBirth)}
+               onChange={handleDateChange}
+        />
+        <button
+          data-test="submit-date-of-birth"
+          className="rounded border-b-4 border-cyan-700 bg-cyan-500 m-2 py-2 px-4 font-bold text-white hover:border-cyan-500 hover:bg-cyan-400"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
