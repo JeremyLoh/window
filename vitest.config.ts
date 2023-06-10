126c6e38f856f50c1254341c0eab490c92a4a716
JeremyLoh <jeremyloh03@gmail.com>
2023-06-10 15:38:15 +0800

    chore: update packages and remove canvas mock

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config"
+import { configDefaults, defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     setupFiles: ["./__tests__/setup.ts"],
+    exclude: [...configDefaults.exclude, "**/setup.ts", "**/serverSetup.ts"],
     environment: "jsdom",
     threads: false, // fix canvas error, https://github.com/vitest-dev/vitest/issues/740
     environmentOptions: {
@@ -14,6 +15,6 @@ export default defineConfig({
       },
     },
     reporters: "verbose",
-    testTimeout: 5000
+    testTimeout: 5000,
   },
 })
