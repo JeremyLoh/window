299f220076fcb9baaa139fd6b146f93ca137b9e3
JeremyLoh <jeremyloh03@gmail.com>
2023-06-18 16:45:53 +0800

    feat: add middleware redirect for dashboard

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,15 @@ import { NextResponse } from "next/server"
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  await supabase.auth.getSession()
+  const response = await supabase.auth.getSession()
+
+  if (
+    !response.data.session &&
+    req.nextUrl.pathname.startsWith("/bugTracker/dashboard")
+  ) {
+    const loginUrl = req.nextUrl.clone()
+    loginUrl.pathname = "/bugTracker/login"
+    return NextResponse.redirect(loginUrl)
+  }
   return res
 }
