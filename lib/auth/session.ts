ef2fdb0bc96fba0a9036db9580ac7053c5079f64
JeremyLoh <jeremyloh03@gmail.com>
2023-06-18 15:12:02 +0800

    chore: move get session function

--- lib/auth/session.ts
+++
@@ -1,7 +0,0 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-
-export async function getSession() {
-  "use server"
-  const supabase = createClientComponentClient()
-  return await supabase.auth.getSession()
-}
--- lib/db/supabaseClient.ts
+++ lib/db/supabaseClient.ts
@@ -6,3 +6,8 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 export function getClient() {
   return createClientComponentClient()
 }
+
+export async function getSession() {
+  const supabase = createClientComponentClient()
+  return await supabase.auth.getSession()
+}
