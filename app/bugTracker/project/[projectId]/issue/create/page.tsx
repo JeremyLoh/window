ea68d1ce9cc36749430c79638d7a8ae254149213
JeremyLoh <jeremyloh03@gmail.com>
2023-07-18 22:43:19 +0800

    refactor: extract "back to project" button

--- app/bugTracker/project/[projectId]/issue/create/page.tsx
+++ app/bugTracker/project/[projectId]/issue/create/page.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link"
-import { ArrowLeftCircleIcon } from "@heroicons/react/24/outline"
-import { Button } from "@/components/ui/button"
 import CreateIssueForm from "../../../../../../components/bugTracker/project/issues/createIssueForm"
+import BackToProject from "../../../../../../components/bugTracker/project/backToProject"
 
 export default function CreateIssuePage({
   params,
@@ -11,14 +9,7 @@ export default function CreateIssuePage({
   const { projectId } = params
   return (
     <div className="flex h-screen w-full flex-col">
-      <Link href={`/bugTracker/project/${projectId}`} className="mt-2 w-fit">
-        <Button className="bg-slate-600 hover:bg-slate-500">
-          <ArrowLeftCircleIcon className="mx-2 h-6 w-6" />
-          <p className="pt-0.5 align-text-bottom text-sm">
-            Back to Project Issues
-          </p>
-        </Button>
-      </Link>
+      <BackToProject projectId={projectId} />
       <h1 className="mx-auto w-full p-2 pt-4 md:w-4/5">Create a New Issue</h1>
       <div className="h-fit">
         <CreateIssueForm projectId={params.projectId} />
