1981560b0ada1260a5f04e9f9f5937c14b5ea914
JeremyLoh <jeremyloh03@gmail.com>
2023-07-25 21:57:16 +0800

    feat: add db function to check for taken username

--- lib/db/user.ts
+++ lib/db/user.ts
@@ -3,3 +3,10 @@ import { getClient } from "./supabaseClient"
 export async function getProfileInfo(id: string) {
   return getClient().from("user").select("username, created_at").eq("id", id)
 }
+
+export async function isUsernameTaken(username: string) {
+  const response = await getClient().rpc("is_username_taken", {
+    username_input: username,
+  }).returns<boolean>()
+  return !response.error && response.data
+}
